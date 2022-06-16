d9f0bd4ec1877ecefd73bf94d9a54bba09c995ec
Robert Waffen <rw@betadots.de>
2022-06-16 15:36:21 +0200

    improvements
    - read cert only once
    - read key only once
    - map ids instead of looping with each
    - use id directly from hash
    - adjust error message

--- lib/puppet/functions/bolt_pe/get_targets_from_node_groups.rb
+++ lib/puppet/functions/bolt_pe/get_targets_from_node_groups.rb
@@ -7,38 +7,37 @@ Puppet::Functions.create_function(:'bolt_pe::get_targets_from_node_groups') do
   end
 
   def run(node_group)
-    value = {}
     # get a list of all existing node groups
     cert   = `/opt/puppetlabs/bin/puppet config print hostcert`.strip
     cacert = `/opt/puppetlabs/bin/puppet config print localcacert`.strip
     key    = `/opt/puppetlabs/bin/puppet config print hostprivkey`.strip
     server = `/opt/puppetlabs/bin/puppet config print server`.strip
 
+    crt_obj = OpenSSL::X509::Certificate.new(File.read(cert))
+    key_obj = OpenSSL::PKey::RSA.new(File.read(key))
+
     all_groups_uri = URI("https://#{server}:4433/classifier-api/v1/groups")
 
     all_groups_http = Net::HTTP.new(all_groups_uri.host, all_groups_uri.port)
     all_groups_http.use_ssl = true
     all_groups_http.ca_file = cacert
-    all_groups_http.cert = OpenSSL::X509::Certificate.new(File.read(cert))
-    all_groups_http.key = OpenSSL::PKey::RSA.new(File.read(key))
+    all_groups_http.cert = crt_obj
+    all_groups_http.key = key_obj
     all_groups_request = Net::HTTP::Get.new all_groups_uri
     all_groups_response = all_groups_http.request(all_groups_request)
 
-    raise StandardError, "Response from #{server} was HTTP #{all_groups_response.code} - #{all_groups_response.message}" unless all_groups_response.code == '200'
+    raise StandardError, "ERROR #{all_groups_response.code} - #{all_groups_response.message}" unless all_groups_response.code == '200'
     all_groups_result = JSON.parse(all_groups_response.body)
-    all_groups_result.each do |element|
-      value[element['name']] = element['id']
-    end
+    ids = all_groups_result.map { |e| [e['name'], e['id']] }.to_h
 
     # Get rule from node group
-    id = value[node_group]
-    get_rule_uri = URI("https://#{server}:4433/classifier-api/v1/groups/#{id}/rules")
+    get_rule_uri = URI("https://#{server}:4433/classifier-api/v1/groups/#{ids[node_group]}/rules")
 
     get_rule_http = Net::HTTP.new(get_rule_uri.host, get_rule_uri.port)
     get_rule_http.use_ssl = true
     get_rule_http.ca_file = cacert
-    get_rule_http.cert = OpenSSL::X509::Certificate.new(File.read(cert))
-    get_rule_http.key = OpenSSL::PKey::RSA.new(File.read(key))
+    get_rule_http.cert = crt_obj
+    get_rule_http.key = key_obj
     get_rule_request = Net::HTTP::Get.new get_rule_uri
     get_rule_response = get_rule_http.request(get_rule_request)
 
@@ -52,8 +51,8 @@ Puppet::Functions.create_function(:'bolt_pe::get_targets_from_node_groups') do
     translate_http = Net::HTTP.new(translate_uri.host, translate_uri.port)
     translate_http.use_ssl = true
     translate_http.ca_file = cacert
-    translate_http.cert = OpenSSL::X509::Certificate.new(File.read(cert))
-    translate_http.key = OpenSSL::PKey::RSA.new(File.read(key))
+    translate_http.cert = crt_obj
+    translate_http.key = key_obj
     translate_request = Net::HTTP::Post.new(translate_uri.request_uri)
     translate_request.body = translate_data
     translate_request['Content-Type'] = 'application/json'
@@ -69,14 +68,15 @@ Puppet::Functions.create_function(:'bolt_pe::get_targets_from_node_groups') do
     puppetdb_http = Net::HTTP.new(puppetdb_uri.host, puppetdb_uri.port)
     puppetdb_http.use_ssl = true
     puppetdb_http.ca_file = cacert
-    puppetdb_http.cert = OpenSSL::X509::Certificate.new(File.read(cert))
-    puppetdb_http.key = OpenSSL::PKey::RSA.new(File.read(key))
+    puppetdb_http.cert = crt_obj
+    puppetdb_http.key = key_obj
     puppetdb_request = Net::HTTP::Get.new(puppetdb_uri)
     puppetdb_response = puppetdb_http.request(puppetdb_request)
 
     raise StandardError, "ERROR #{puppetdb_response.code} - #{puppetdb_response.message}" unless puppetdb_response.code == '200'
     puppetdb_result = JSON.parse(puppetdb_response.body)
 
+    # return array of certnames
     puppetdb_result.map { |element| element['certname'] }
   end
 end
